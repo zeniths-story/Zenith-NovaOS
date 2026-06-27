cd11b79be81a0c5670f14f0ea5fa3b260c5a1394
zeniths-story <mmathfam07@gmail.com>
2026-06-27 20:02:56 +0000

    v0.0.12

--- script.js
+++ script.js
@@ -1,109 +1,84 @@
 
+//time in topbar - works; don't move from top
+setInterval(function () {
+    document.querySelector("#timeOfEarth").innerHTML = new Date().toLocaleString();
+}, 1000);
+
+
 
 //opening and closing
 var welcomeScreen = document.querySelector("#welcome");
+
 function closeWindow(element) {
     element.style.display = "none";
 }
+
 function openWindow(element){
     element.style.display = "flex";
 }
+
 var welcomeScreenClose = document.querySelector("#welcomeclose");
 var welcomeScreenOpen = document.querySelector("#welcomeopen");
 
-welcomeScreenClose.addEventListener("click", function(){
-    closeWindow(welcomeScreen);
-});
 
-welcomeScreenOpen.addEventListener("click", function(){
-    closeWindow(welcomeScreen);
-});
+//welcomeScreenClose.addEventListener("click", function () {
+    //closeWindow(welcomeScreen);
+//});
 
-//time in topbar - works; don't touch
-setInterval(function (){
-    document.querySelector("#timeOfEarth").innerHTML = new Date().toLocaleString();
-}, 1000)
+//document.querySelector("#welcomeclose").addEventListener("click", function () {
+    //closeWindow(document.querySelector("#welcome"));
+//});
+
+welcomeScreenClose.addEventListener("click", closeWindow(welcomeScreen));
+
+
+
+//welcomeScreenOpen.addEventListener("click", function () {
+    //openWindow(welcomeScreen);
+//});
+
+//document.querySelector("#welcomeopen").addEventListener("click", function () {
+    //openWindow(document.querySelector("#welcome"));
+//});
+
+welcomeScreenOpen.addEventListener("click", openWindow(welcomeScreen));
+
+
 
 //dragging elements - buggy; needs fixed
 
-//dragElement(document.getElementById("welcome"));
+//dragElement(document.getElementById("welcome")); //
 
-//function dragElement(element) {
-   // var initX = 0;
-//var initY = 0;
-   // var currX = 0;
-    //var currY = 0;
-    //if (document.getElementById(element.id + "header")){
-        //document.getElementById(element.id + "header").onmousedown = startDragging;
-    //}else{
-     //   element.onmousedown = startDragging;
-    }
+//function dragElement(elmnt) {
+  //var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  //if (document.getElementById(elmnt.id + "header")) {
+   // document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  //} else {
+    //elmnt.onmousedown = dragMouseDown;
+  //}
 
- // function startDragging(e) {
-    //  e = e || window.event;
-    //  e.preventDefault();
-    //  initX = e.clientX;
-     // initY = e.clientY;
-     // document.onmouseup = stopDragging;
-     // document.onmousemove = dragElement;
-  }
+  //function dragMouseDown(e) {
+    //e = e || window.event;
+    //e.preventDefault();
+    //pos3 = e.clientX;
+    //pos4 = e.clientY;
+    //document.onmouseup = closeDragElement;
+    //document.onmousemove = elementDrag;
+ // }
 
- // function dragElement(e){
-    //  e = e || window.event;
-     // e.preventDefault();
-     // currX = initX - e.clientX;
-     // currY = initY - e.clientY;
-     // initX = e.clientX;
-     // initY = e.clientY;
-     // element.style.top = (element.offsetTop - currY) + "px";
-     // element.style.left = (element.offsetLeft - currX) + "px";
-  }
+  //function elementDrag(e) {
+   // e = e || window.event;
+   // e.preventDefault();
+    //pos1 = pos3 - e.clientX;
+    //pos2 = pos4 - e.clientY;
+    //pos3 = e.clientX;
+    //pos4 = e.clientY;
+    //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+   // elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+ // }
 
- // function stopDragging () {
-      //document.onmouseup = null;
-      //document.onmousemove = null;
-  }
-}
-
-dragElement(document.getElementById("welcome"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+  //function closeDragElement() {
+    //document.onmouseup = null;
+    //document.onmousemove = null;
+  //}
+//}
