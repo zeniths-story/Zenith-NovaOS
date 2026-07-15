5594fd179e74e0452596dd34c3125c8cecf74c6f
zeniths-story <mmathfam07@gmail.com>
2026-07-15 00:14:09 +0000

    v0.0.19

--- script.js
+++ script.js
@@ -4,104 +4,27 @@ setInterval(function () {
     document.querySelector("#timeOfEarth").innerHTML = new Date().toLocaleString();
 }, 1000);
 
-var topBar = document.querySelector("#top")
-
-//clicktest
-//document.getElementById("wlcmeclose").onclick =test();
-  
-function test() {
-    closeWindow(welcomeScreen);
-    document.getElementById("test").innerHTML= "success";
-  
-};
-
-//rand
+var topBar = document.getElementById("top")
 var highindx = 1;
 
-function addWindTapHand(element){
-  element.addEventListener("mousedown", () => handWindTap(element) 
- )
-};
-
-function handWindTap (element) {
-  highindx++;
-  element.style.zIndex = highindx
-  deselectIcon(slectIcon)
-};
-
-function initwind(elementName) {
-  var screen = document.querySelector("#"+ elementName);
-  addWindTapHand(screen)
-  dragElement(screen)
-//makeCloseable(elementName);
-}
-
-initwind(notesScreen)
-
-//opening and closing - does not work currently
-
-//close def.
-var welcomeScreenClose = document.querySelector("#wlcmeclose");
-var notesclose = document.querySelector("#novanotesclose")
-
-function closeWindow(element) {
-    element.style.display = "none";
-
-};
-
-//open def.
-var welcomeScreenOpen = document.querySelector("#wlcmeopen");
-
-function openWindow(element){
-    element.style.display = "flex";
-    highindx++;
-    element.style.zIndex = highindx;
-    topBar.style.zIndex = highindx + 1
-};
-
 //defining screens
-var welcomeScreen = document.querySelector("#wlcmescrn");
-var notesScreen = document.querySelector("#NovaNotes");
-
-//open/close commands
-welcomeScreenClose.addEventListener("click", function () {
-    closeWindow(welcomeScreen);
-});
-
-welcomeScreenOpen.addEventListener("click", function () {
-    openWindow(welcomeScreen);
-});
+var welcomeScreen = document.getElementById("wlcmescrn");
+var notesScreen = document.getElementById("NovaNotes");
 
 
-
-
-
-
-
-
-
-
-//dragging elements - buggy; needs fixed
-//elements we (are trying to) drag
+//dragging elements
+//elements we can drag
 dragElement(welcomeScreen);
-dragElement(document.querySelector("#NovaNotes"));
-
+dragElement(notesScreen);
 
 //defining dragElement
 function dragElement(element) {
-
   var currX = 0, currY = 0, initX = 0, initY = 0;
-
   if (document.getElementById(element.id + "header")) {
-
     document.getElementById(element.id + "header").onmousedown = startDrag;
-
   } else {
-
     element.onmousedown = startDrag;
-
   };
-
   function startDrag(e) {
     e = e || window.event;
     e.preventDefault();
@@ -110,7 +33,6 @@ function dragElement(element) {
     document.onmouseup = stopDrag;
     document.onmousemove = dragElement;
   };
-
   function dragElement(e) {
     e = e || window.event;
     e.preventDefault();
@@ -118,20 +40,14 @@ function dragElement(element) {
     currY = initY - e.clientY;
     initX = e.clientX;
     initY = e.clientY;
-    element.StyleSheet.top = (element.offsetTop - currY) + "px";
-    element.StyleSheet.left = (element.offsetLeft - currX) + "px";
+    element.style.top = (element.offsetTop - currY) + "px";
+    element.style.left = (element.offsetLeft - currX) + "px";
   };
-
   function stopDrag() {
     document.onmouseup = null;
     document.onmousemove = null;
   };
 };
-
-
-
-
-
 
 //app stuff
 var slectIcon = undefined;
@@ -155,3 +71,71 @@ function handleIconSlect(element) {
   };
   
 };
+
+//opening and closing
+
+//close def.
+var welcomeScreenClose = document.getElementById("wlcmeclose");
+var notesclose = document.getElementById("novanotesclose")
+
+function closeWindow(element) {
+    element.style.display = "none";
+
+};
+
+//open def.
+var welcomeScreenOpen = document.getElementById("wlcmeopen");
+
+function openWindow(element){
+    element.style.display = "flex";
+    highindx++;
+    element.style.zIndex = highindx;
+    topBar.style.zIndex = highindx + 1
+};
+
+
+
+//open/close commands
+welcomeScreenClose.addEventListener("click", function () {
+    closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function () {
+    openWindow(welcomeScreen);
+});
+
+
+notesclose.addEventListener("click", function () {
+    closeWindow(notesScreen);
+});
+
+
+
+
+//window tap handling
+
+function handWindTap (element) {
+  highindx++;
+  element.style.zIndex = highindx
+  deselectIcon(slectIcon)
+};
+
+function addWindTapHand(element){
+  element.addEventListener("click", function(){ 
+    handWindTap(element)
+  });
+};
+
+//initilizing windows
+function initwind(elementName) {
+  var screen = document.querySelector("#"+ elementName);
+  addWindTapHand(screen)
+  dragElement(screen)
+  //makeCloseable(elementName);
+}
+
+
+
+
+
+//initwind(notesScreen)
